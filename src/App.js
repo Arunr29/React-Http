e5fc312f61960196b87c4a37e2d27bca80d9d10a
arun@makolet.in <password9nine>
2023-04-04 18:54:21 +0530

    added 3 types of conditional content

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   //       throw new Error('Something went wrong');
   //     }
   //     const data = await response.json();
-    
+
 
   //     console.log(data.results)
   //     const transformData = data.results.map(value => {
@@ -68,16 +68,30 @@ function App() {
   //   setLoading(false);
   // }
 
+  //this is a great example where should use let and const
+  let content = <p> Found no movies...</p>
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+  if (error) {
+    content = <p>{error}</p>
+  }
+  if (isLoading) {
+ content = <p>Loading..</p>
+  }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && movies.length > 0 ? <MoviesList movies={movies} /> : ''}
-        {!isLoading && movies.length == 0? < p > Found no movies...</p> : ''}
+        {content}
+        {/* {!isLoading && movies.length > 0 ? <MoviesList movies={movies} /> : ''}
+        {!isLoading && movies.length == 0 ? < p > Found no movies...</p> : ''}
         {isLoading && !error ? <p>Loading...</p> : ''}
-        {!isLoading && error ? '' : <p>{error}</p>}
+        {!isLoading && error ? '' : <p>{error}</p>} */}
         {/* alternate */}
         {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length == 0 && !error && <p>Found no movies...</p>}
